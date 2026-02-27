f19e679270d3796270817eccf154d9b8ad752af4
Ajinkya <ajinkya@carbonmius.com>
2026-02-28 01:12:40 +0530

    [closes #00]: static web deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/',
+  base: '/liferootsciences/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
